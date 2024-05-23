8ab83792f1ad7f2254afadc71bf856d56e68fa46
GiuseppeDeRosa1992 <derosagiuseppe1092@gmail.com>
2024-05-23 16:07:34 +0200

    added header with AppHeader.vue with cycle v-for for link

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -2,7 +2,48 @@
 export default {
     data() {
         return {
-
+            links: [
+                {
+                    text: "CHARACTERS",
+                    url: "#CHARACTERS",
+                },
+                {
+                    text: "COMICS",
+                    url: "#COMICS",
+                },
+                {
+                    text: "MOVIES",
+                    url: "#MOVIES",
+                },
+                {
+                    text: "TV",
+                    url: "#TV",
+                },
+                {
+                    text: "GAMES",
+                    url: "#GAMES",
+                },
+                {
+                    text: "COLLECTIBLES",
+                    url: "#COLLECTIBLES",
+                },
+                {
+                    text: "VIDEOS",
+                    url: "#VIDEOS",
+                },
+                {
+                    text: "FANS",
+                    url: "#FANS",
+                },
+                {
+                    text: "NEWS",
+                    url: "#NEWS",
+                },
+                {
+                    text: "SHOP",
+                    url: "#SHOP",
+                },
+            ],
         }
     },
 
@@ -17,9 +58,28 @@ export default {
 </script>
 
 <template>
-    <header>
-        <h2>Ciao sono Header</h2>
+    <header class="py-2 bg-white text-dark">
+        <div class="container m-auto">
+            <div class="row m-0 justify-content-between align-items-center">
+                <figure id="logo" class="col-1 p-0 m-0">
+                    <img src="../../public/dc-logo.png" alt="" class="w-75">
+                </figure>
+                <nav class="col-9 p-0">
+                    <ul class="row m-0 p-0 justify-content-between">
+                        <li v-for="link in links" class="col-1 p-0 text-center my-fs">
+                            <a class="text-dark text-decoration-none" :href="link.url">
+                                <b>{{ link.text }}</b>
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
     </header>
 </template>
 
-<style></style>
+<style scoped>
+.my-fs {
+    font-size: 0.7rem;
+}
+</style>
