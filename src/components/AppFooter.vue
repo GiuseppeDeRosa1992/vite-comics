500c16d94236e0e13804ca52642a98444a782267
GiuseppeDeRosa1992 <derosagiuseppe1092@gmail.com>
2024-05-23 17:40:47 +0200

    added footer in footer with img and v-for

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -2,7 +2,7 @@
 export default {
     data() {
         return {
-            images: [
+            cards: [
                 {
                     text: "DIGITAL COMICS",
                     search: "./card-dc.png",
@@ -24,6 +24,24 @@ export default {
                     search: "./dc-power-visa.svg",
                 },
             ],
+
+            images: [
+                {
+                    src: "./footer-facebook.png",
+                },
+                {
+                    src: "./footer-twitter.png",
+                },
+                {
+                    src: "./footer-youtube.png",
+                },
+                {
+                    src: "./footer-pinterest.png",
+                },
+                {
+                    src: "./footer-periscope.png",
+                },
+            ],
         }
     },
 
@@ -38,10 +56,12 @@ export default {
 </script>
 
 <template>
+    <!-- Suddivido Footer in 3 sezioni header main e footer -->
     <footer>
+        <!-- Aggiuto header del footer con le img e il background azzurro -->
         <header class="bg-primary py-3">
             <div class="container row m-auto justify-content-evenly align-items-center">
-                <div class="col-2" v-for="card in images">
+                <div class="col-2" v-for="card in cards">
                     <div class="row m-0 align-items-center py-3">
                         <figure class="col-4 m-0">
                             <img :src="card.search" alt="" class="img-fluid">
@@ -53,13 +73,27 @@ export default {
                 </div>
             </div>
         </header>
-        <main>
+        <!-- aggiungo il main del footer con background image e link vari -->
+        <main class="py-3">
 
         </main>
-        <footer>
-
+        <!--  aggiungo footer del footer con bottone a sinistra e a destra le varie img social -->
+        <footer class="py-4 bg-footer">
+            <div class="container row m-auto justify-content-between align-items-center">
+                <div class="col-2">
+                    <button type="button" class="btn btn-outline-info text-white">SIGN-UP NOW!</button>
+                </div>
+                <div class="col-8 text-end">
+                    <span class="text-primary p-1">FOLLOW US</span>
+                    <img :src="img.src" alt="" v-for="img in images" class="px-2">
+                </div>
+            </div>
         </footer>
     </footer>
 </template>
 
-<style></style>
+<style>
+.bg-footer {
+    background-color: #303030;
+}
+</style>
